23f3811d00fc26c50ac01c6639bbbbc61c9b1548
Daria Artiomenko <daridaraart@gmail.com>
2024-07-15 18:04:46 +0300

    add logic in timer

--- src/pages/quizQuestionPage/QuizQuestionPage.tsx
+++ src/pages/quizQuestionPage/QuizQuestionPage.tsx
@@ -34,74 +34,24 @@ const questions = [
     { questionId: 5, answerIds: [ 2, 3, 5, 6] },
     { questionId: 6, answerIds: [5, 6] },
   ];
-// const quizQuestions = [
-//     {
-//       id: 1,
-//       question: 'What is the capital of France?',
-//       answers: [
-//         { option: 'Paris', isCorrect: true },
-//         { option: 'London', isCorrect: false },
-//         { option: 'Berlin', isCorrect: false },
-//         { option: 'Madrid', isCorrect: false },
-//       ],
-//       type: 'multiple',
-//       category: 'Geography',
-//     },
-//     {
-//       id: 2,
-//       question: 'The Great Pyramid of Giza is located in which country?',
-//       answers: [
-//         { option: 'Egypt', isCorrect: true },
-//         { option: 'Greece', isCorrect: false },
-//       ],
-//       type: 'boolean',
-//       category: 'History',
-//     },
-//     {
-//       id: 3,
-//       question: 'What is the largest ocean on Earth?',
-//       answers: [
-//         { option: 'Pacific Ocean', isCorrect: true },
-//         { option: 'Atlantic Ocean', isCorrect: false },
-//         { option: 'Indian Ocean', isCorrect: false },
-//         { option: 'Arctic Ocean', isCorrect: false },
-//       ],
-//       type: 'multiple',
-//       category: 'Geography',
-//     },
-//     {
-//       id: 4,
-//       question: 'The Mona Lisa was painted by which artist?',
-//       answers: [
-//         { option: 'Leonardo da Vinci', isCorrect: true },
-//         { option: 'Michelangelo', isCorrect: false },
-//       ],
-//       type: 'boolean',
-//       category: 'Art & Literature',
-//     },
-//     {
-//       id: 5,
-//       question: 'What is the smallest planet in our solar system?',
-//       answers: [
-//         { option: 'Mercury', isCorrect: true },
-//         { option: 'Venus', isCorrect: false },
-//         { option: 'Earth', isCorrect: false },
-//         { option: 'Mars', isCorrect: false },
-//       ],
-//       type: 'multiple',
-//       category: 'Science',
-//     },
-//   ];
+
 
 export const QuizQuestionPage: React.FC = () => {
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
     const [selectedAnswer, setSelectedAnswer] = useState<string[]>([]);
     const [quizTime, setQuizTime] = useState(120);
+    const [isQuizOver, setIsQuizOver] = useState(false); 
 
     useEffect(() => {
         const timer = setInterval(() => {
-          setQuizTime((prevTime) => prevTime - 1);
-        }, 1000);
+            setQuizTime((prevTime) => {
+              if (prevTime === 0) {
+                setIsQuizOver(true); 
+                return 0; 
+              }
+              return prevTime - 1;
+            });
+          }, 1000);
     
         return () => clearInterval(timer);
     }, []);
@@ -119,6 +69,7 @@ export const QuizQuestionPage: React.FC = () => {
         setSelectedAnswer([]);
         setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
     };
+    
 
     const handleAnswerSelect = (answer: string) => {
         setSelectedAnswer((prevSelectedAnswer) =>
@@ -129,31 +80,35 @@ export const QuizQuestionPage: React.FC = () => {
     };
     
     const handleEndQuiz = () => {
-    // 
-    };
+        setIsQuizOver(true); 
+      };
 
     return (
         <>
-            <Timer time={quizTime} />
-            <Question
-                key={currentQuestion.id}
-                question={currentQuestion.text}
-                category={currentQuestion.category}
-                type={currentQuestion.type as "boolean" | "multiple"}
-                answers={answerOptionsForCurrentQuestion}
-                selectedAnswers={selectedAnswer}
-                onAnswerSelect={handleAnswerSelect}
-            />
-
-            <div className='flex gap-4 mb-16 justify-center'>
-                    <ButtonMain onClick={handleNextQuestion} label="Next"/>
-                    <ButtonSecondary onClick={handleEndQuiz} label="End Quiz" styles='w-32 h-12 mt-10'/>
-            </div>
-
-            <ProgressBar
-                currentIndex={currentQuestionIndex}
-                totalQuestions={questions.length}
-            />
+            {!isQuizOver && (
+                <>
+                    <Timer time={quizTime} onTimeUp={handleEndQuiz} />
+                    <Question
+                        key={currentQuestion.id}
+                        question={currentQuestion.text}
+                        category={currentQuestion.category}
+                        type={currentQuestion.type as "boolean" | "multiple"}
+                        answers={answerOptionsForCurrentQuestion}
+                        selectedAnswers={selectedAnswer}
+                        onAnswerSelect={handleAnswerSelect}
+                    />
+    
+                    <div className='flex gap-4 mb-16 justify-center'>
+                            <ButtonMain onClick={handleNextQuestion} label="Next"/>
+                            <ButtonSecondary onClick={handleEndQuiz} label="End Quiz" styles='w-32 h-12 mt-10'/>
+                    </div>
+                    <ProgressBar
+                        currentIndex={currentQuestionIndex}
+                        totalQuestions={questions.length}
+                    />
+                </>
+            )}
+            {isQuizOver && <div className='text-3xl font-medium text-amber-500 mb-16 text-center w-2/3 mx-auto'>Quiz is over!</div>}
         </>
     )
 }
