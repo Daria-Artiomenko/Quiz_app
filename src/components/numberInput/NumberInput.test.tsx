c28793e016b32a5c910d165baacca70628ae1d8d
Daria Artiomenko <daridaraart@gmail.com>
2024-08-22 18:00:56 +0300

    extracted repeated code to the setup function

--- src/components/numberInput/NumberInput.test.tsx
+++ src/components/numberInput/NumberInput.test.tsx
@@ -1,47 +1,35 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import NumberInput from "./NumberInput";
+
+const mockOnChange = jest.fn();
+const setup = () =>
+    render(
+        <NumberInput
+            label='Number of Questions'
+            value='10'
+            onChange={mockOnChange}
+            min={5}
+            max={15}
+        />
+    );
+
 describe("NumberInput", () => {
     it("should render the label correctly", () => {
-        render(
-            <NumberInput
-                label='Number of Questions'
-                value='10'
-                onChange={() => {}}
-                min={5}
-                max={15}
-            />
-        );
+        setup();
         expect(
             screen.getByLabelText("Number of Questions:")
         ).toBeInTheDocument();
     });
 
     it("should render the input with the correct value", () => {
-        render(
-            <NumberInput
-                label='Number of Questions'
-                value='10'
-                onChange={() => {}}
-                min={5}
-                max={15}
-            />
-        );
+        setup();
         expect((screen.getByRole("spinbutton") as HTMLInputElement).value).toBe(
             "10"
         );
     });
 
     it("should call the onChange callback when the input value changes", () => {
-        const mockOnChange = jest.fn();
-        render(
-            <NumberInput
-                label='Number of Questions'
-                value='10'
-                onChange={mockOnChange}
-                min={5}
-                max={15}
-            />
-        );
+        setup();
         fireEvent.change(screen.getByRole("spinbutton"), {
             target: { value: "12" },
         });
